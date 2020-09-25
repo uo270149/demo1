10015235ec45162020671787ac55f6e79321e125
Sofía Suárez Fernández <sofiasuarezfdez@gmail.com>
2020-09-25 14:13:05 +0200

    Añadir líneas al método main

--- src/demo1/Demo.java
+++ src/demo1/Demo.java
@@ -1,5 +1,9 @@
 package demo1;
 
 public class Demo {
-
+	public static void main(String[] args) {
+		System.out.println("línea 1");
+		System.out.println("línea 2");
+		System.out.println("línea 3");
+	}
 }
